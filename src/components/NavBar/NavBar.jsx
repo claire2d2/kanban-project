7fb28bf2879c398deb08418471f330c18fa1b1b2
Claire Song <claireyuan.song@gmail.com>
2024-03-20 16:28:18 +0100

    corrected light mode

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     //   <p>Yes we Kanban!</p>
     // </nav>
 
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="border-gray-200 bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
